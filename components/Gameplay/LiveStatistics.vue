8e9fa71592febffc79dbcb98ab61c11c4541f905
valenciaga1231 <martin.valencic1999@gmail.com>
2023-09-29 14:08:00 +0200

    add hearts lives and logic to live results

--- components/Gameplay/LiveStatistics.vue
+++ components/Gameplay/LiveStatistics.vue
@@ -13,7 +13,16 @@
                 <!-- <TransitionGroup name="list" tag="tr"> -->
                 <tr v-for="(value, index) in results" :key="index" class="bg-white border-b">
                     <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">{{ index }}</th>
-                    <td class="px-6 py-4">{{ value.lives }}</td>
+                    <td class="px-6 py-4">
+                        <div v-for="life in total_attempts.get(index)" :key="life" style="display: inline-block">
+                            <div v-if="value.lives >= life">
+                                <SvgsHeartIcon class="svg-heart-icon" :color="'#FF0000'" />
+                            </div>
+                            <div v-else>
+                                <SvgsHeartIcon class="svg-heart-icon" :color="'#8e7777'" />
+                            </div>
+                        </div>
+                    </td>
                     <td class="px-6 py-4">{{ processDistance(value.distance) }}</td>
                     <td class="px-6 py-4">{{ value.baseScr || 0 }}</td>
                 </tr>
@@ -27,19 +36,41 @@
 export default {
     setup() {
         const results = useResults();
+        const game_flow = useGameFlow();
+        const total_attempts = ref(new Map<string | number, number>());
 
+        /**
+         * Process distance to display in results table
+         * @param distance Distance in meters
+         * @returns Distance in meters or km
+         */
         const processDistance = (distance: number | undefined): string => {
             if (!distance) return "--"; // This means no guess yet from player
             const distanceInMeters = distance < 1000 ? distance : distance / 1000; // Convert to km unit if neccesary
             return `${Math.round(distanceInMeters * 10) / 10} ${distance < 1000 ? "m" : "km"}`; // Return distance in corret units
         };
 
-        return { results, processDistance };
+        /**
+         * Function creates total attempts constant variable for every player
+         * at the start of every round. This is used to display lives in results table.
+         */
+        const createTotalAttempts = () => {
+            // Create an array map of total lives for every player
+            for (const player_id in results.value) total_attempts.value.set(player_id, results.value[player_id].lives);
+        };
+
+        // Watch game flow to create total attempts
+        watch(game_flow, (new_val) => (new_val === "PLAYING" ? createTotalAttempts() : null));
+
+        return { results, total_attempts, processDistance };
     },
 };
 </script>
 
 <style scoped>
+.svg-heart-icon {
+    width: 12px;
+}
 /* Live statistics animation */
 .list-move, /* apply transition to moving elements */
 .list-enter-active,
