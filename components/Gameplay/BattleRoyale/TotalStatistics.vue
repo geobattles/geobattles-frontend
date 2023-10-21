43166668be4b3c7b259585dfc7f97dc406cef04c
valenciaga1231 <martin.valencic1999@gmail.com>
2023-10-21 22:52:06 +0200

    add total score table in midround for BR

---
+++ components/Gameplay/BattleRoyale/TotalStatistics.vue
@@ -0,0 +1,105 @@
+<template>
+    <div class="statistics">
+        <div class="table__row winner">
+            <div class="table__row-element" style="flex: 10%">Place</div>
+            <div class="table__row-element" style="flex: 20%"><span>Name</span></div>
+            <div class="table__row-element" style="flex: 20%">&nbsp;</div>
+            <div class="table__row-element">Score</div>
+        </div>
+        <TransitionGroup name="list">
+            <div v-for="(value, player_id, index) in total_results" :key="player_id" class="table__row" :class="[index === 0 ? 'winner' : '', index === 1 ? 'second' : '', index === 2 ? 'third' : '']">
+                <div class="table__row-element" style="flex: 10%">
+                    <SvgsTrophyIcon :color="trophy_color[index]" width="30" />
+                </div>
+                <div class="table__row-element" style="flex: 20%">
+                    <div class="player-name">
+                        {{ getPlayerNameFromID(player_id) }}
+                    </div>
+                </div>
+                <div class="table__row-element" style="flex: 20%">
+                    <SvgsUserIcon :color="getPlayerColorByID(player_id)" :width="20" />
+                </div>
+                <div class="table__row-element"><AnimateScore :startAmount="0" :endAmount="value.total" :duration="4" separator="" :autoinit="true" /></div>
+                <!-- <PowerupDuel v-if="setting_store.lobby_settings.powerups[1] == true" :player_id="value[0]" /> -->
+            </div>
+        </TransitionGroup>
+    </div>
+</template>
+
+<script>
+export default {
+    setup() {
+        const trophy_color = ["gold", "silver", "#CD7F32"];
+        const total_results = useTotalResults();
+
+        return { trophy_color, total_results };
+    },
+};
+</script>
+
+<style scoped>
+.statistics {
+    width: 100%;
+    padding: 0px 0px;
+}
+.table__row {
+    display: flex;
+
+    padding: 10px;
+    margin: auto;
+    margin-bottom: 10px;
+
+    width: 85%;
+
+    position: relative;
+}
+
+.table__row-element {
+    flex: 50%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 5px;
+    margin: 0px 10px;
+
+    margin: auto;
+}
+
+.player-name {
+    display: block;
+    overflow: hidden;
+    white-space: nowrap;
+    max-width: 150px;
+}
+
+.winner {
+    width: 95%;
+}
+
+.second {
+    width: 92.5%;
+}
+
+.third {
+    width: 90%;
+}
+
+/* LIVE STATS ANIMATION LIST */
+.list-move, /* apply transition to moving elements */
+.list-enter-active,
+.list-leave-active {
+    transition: all 0.8s linear;
+}
+
+.list-enter-from,
+.list-leave-to {
+    opacity: 0;
+    transform: translateX(45px);
+}
+
+/* ensure leaving items are taken out of layout flow so that moving
+   animations can be calculated correctly. */
+.list-leave-active {
+    position: absolute;
+}
+</style>
--- components/Gameplay/MidRoundView.vue
+++ components/Gameplay/MidRoundView.vue
@@ -1,20 +1,7 @@
 <template>
     <div id="midround_container">
-        <div class="total-result-table relative overflow-x-auto shadow-md sm:rounded-lg bg-white">
-            <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-                <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                    <tr>
-                        <th scope="col" class="px-6 py-3">Name</th>
-                        <th scope="col" class="px-6 py-3">Score</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr v-for="(value, index) in total_results">
-                        <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">{{ index }}</th>
-                        <td class="px-6 py-4">{{ value.total || 0 }}</td>
-                    </tr>
-                </tbody>
-            </table>
+        <div class="total-result-table">
+            <GameplayBattleRoyaleTotalStatistics v-if="game_type === 'BattleRoyale'" />
         </div>
         <div class="midround-menu">
             <button @click="next_round" type="button" class="btn btn-blue">Next round</button>
@@ -27,9 +14,10 @@ export default {
     setup() {
         const total_results = useTotalResults();
         const next_round = Gameplay.nextRound;
+        const game_type = useGameType();
 
         onMounted(() => {});
-        return { total_results, next_round };
+        return { total_results, next_round, game_type };
     },
 };
 </script>
@@ -52,7 +40,12 @@ export default {
 }
 
 .total-result-table {
-    @apply max-w-lg;
+    position: relative;
+
+    background-color: white;
+    color: black;
+
+    max-width: 600px;
     z-index: 1;
 }
 </style>
