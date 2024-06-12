23f6efe13635beff184979a6888095a063af66e2
valenciaga <martin.valencic1999@gmail.com>
2024-06-12 15:42:30 +0200

    add asynchronous loading for background and maps api

--- app.vue
+++ app.vue
@@ -1,39 +1,49 @@
 <template>
-    <div id="vue-app">
+    <div ref="vue_app" id="vue-app">
         <NuxtPage />
     </div>
 </template>
 
-<script>
+<script lang="ts">
 export default {
     setup() {
+        const vue_app = ref<HTMLElement | null>(null);
+
         if (process.server) {
             // Read ENV variables
             const runtimeConfig = useRuntimeConfig();
             console.log("App.vue was SSR-ed"); //! Dev
-            // console.log(`https://maps.googleapis.com/maps/api/js?key=${runtimeConfig.public.GMAPS_API}&v=weekly`); //! Dev
-            const script = `https://maps.googleapis.com/maps/api/js?key=${runtimeConfig.public.GMAPS_API}&v=weekly`;
+            const script = `https://maps.googleapis.com/maps/api/js?key=${runtimeConfig.public.GMAPS_API}&v=weekly&loading=async`;
 
             useHead({
-                script: [{ src: script, defer: true, async: true }],
+                script: [{ src: script, async: true, defer: true }],
                 title: `GeoBattles`,
             });
+
             useBackendAPI().value = runtimeConfig.public.DEV_BACKEND_API_HOST;
         }
 
         if (process.client) {
             const saved_username = useCookie("saved_username");
-            console.log("saved_username.value: ", saved_username.value); //! Dev
-            usePlayerInfo().value.name = saved_username.value;
+            console.log("saved_username: ", saved_username.value); //! Dev
+            if (typeof saved_username.value === "string") usePlayerInfo().value.name = saved_username.value;
+
+            const img = new Image();
+            img.src = "/images/earth.webp";
+            img.onload = () => (vue_app.value !== null ? (vue_app.value.style.backgroundImage = "url(/images/earth.webp)") : "");
         }
+
+        return { vue_app };
     },
 };
 </script>
 
 <style>
-body {
-    /* Set background image */
-    background: url("/images/earth.webp") no-repeat center center fixed;
-    background-size: cover; /* Resize the background image to cover the entire container */
+#vue-app {
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: cover;
+    height: 100%;
+    min-height: 100vh;
 }
 </style>
