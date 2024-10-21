3b0e8759de759cca4590df0efce5fb613ab9a54d
valenciaga1231 <martin.valencic1999@gmail.com>
2024-10-22 00:43:03 +0200

    append map and panorama back to dom after new game

--- components/Gameplay/GoogleMap.vue
+++ components/Gameplay/GoogleMap.vue
@@ -12,8 +12,16 @@ export default {
 
         onMounted(() => {
             if (!google_map.value) throw new Error("Google Map DOM element not found");
-            useGoogleMapHTML().value = google_map.value;
-            initalizeNewGoogleMap(google_map.value); // Init Google Map
+            const existingMap = useGoogleMapHTML().value;
+            console.log("Existing Google Map", existingMap);
+            if (existingMap) {
+                console.log("Google Map HTML already exists");
+                google_map.value.appendChild(existingMap);
+            } else {
+                console.log("Google Map HTML does not exist so creating new one");
+                useGoogleMapHTML().value = google_map.value;
+                initalizeNewGoogleMap(google_map.value); // Init Google Map
+            }
         });
 
         return { google_map, props };
--- components/Gameplay/GooglePanorama.vue
+++ components/Gameplay/GooglePanorama.vue
@@ -9,8 +9,16 @@ export default {
 
         onMounted(() => {
             if (!google_panorama.value) throw new Error("Google Panorama DOM element not found");
-            useGooglePanoramaHTML().value = google_panorama.value;
-            initalizeNewPanoramaView(google_panorama.value); // Init Google Map
+            const existingPanorama = useGooglePanoramaHTML().value;
+            console.log("Existing Panorama", existingPanorama);
+            if (existingPanorama) {
+                console.log("Google Panorama HTML already exists");
+                google_panorama.value.appendChild(existingPanorama);
+            } else {
+                console.log("Google Panorama HTML does not exist so creating new one");
+                useGooglePanoramaHTML().value = google_panorama.value;
+                initalizeNewPanoramaView(google_panorama.value); // Init Google Map
+            }
         });
 
         return { google_panorama };
