39541f4f07ddd98a5adf019a17c0979f1c006759
valenciaga <martin.valencic1999@gmail.com>
2024-11-10 23:21:08 +0100

    add option to join by link

--- middleware/dev.global.ts
+++ middleware/dev.global.ts
@@ -1,5 +1,5 @@
 export default defineNuxtRouteMiddleware((to, from) => {
-    const allowedPaths = ["/", "/signup", "/lobby/list", "/about"];
+    const allowedPaths = ["/", "/signup", "/lobby/list", "/lobby/join", "/about"];
     if (!allowedPaths.includes(to.path)) {
         if (!usePlayerInfo().value.ID) {
             return navigateTo("/");
--- pages/lobby/join.vue
+++ pages/lobby/join.vue
@@ -1,131 +1,140 @@
 <template>
-    <div class="page-container">
-        <div class="inputs-parent" ref="input_parent">
-            <input ref="input_1" class="sign-input" @keyup="handleKeyUpEvent($event)" @keydown="handleKeyDownEvent($event)" type="text" />
-            <input ref="input_2" class="sign-input" @keyup="handleKeyUpEvent($event)" @keydown="handleKeyDownEvent($event)" type="text" />
-            <input ref="input_3" class="sign-input" @keyup="handleKeyUpEvent($event)" @keydown="handleKeyDownEvent($event)" type="text" />
-            <input ref="input_4" class="sign-input" @keyup="handleKeyUpEvent($event)" @keydown="handleKeyDownEvent($event)" type="text" />
-            <input ref="input_5" class="sign-input" @keyup="handleKeyUpEvent($event)" @keydown="handleKeyDownEvent($event)" type="text" />
-            <input ref="input_6" class="sign-input" @keyup="handleKeyUpEvent($event)" @keydown="handleKeyDownEvent($event)" type="text" />
-        </div>
-        <div>
-            <span style="color: white">Insert or paste lobby code to join the game!</span>
+    <div class="h-screen">
+        <Toast position="bottom-right" />
+        <Header class="absolute w-full" />
+        <div class="flex flex-col justify-center items-center h-full bg-p-surface-900">
+            <div class="flex flex-wrap justify-center scale-75 lg:scale-100" ref="input_parent">
+                <input ref="input_1" class="sign-input" @keyup="handleKeyUpEvent($event)" @keydown="handleKeyDownEvent($event)" type="text" />
+                <input ref="input_2" class="sign-input" @keyup="handleKeyUpEvent($event)" @keydown="handleKeyDownEvent($event)" type="text" />
+                <input ref="input_3" class="sign-input" @keyup="handleKeyUpEvent($event)" @keydown="handleKeyDownEvent($event)" type="text" />
+                <input ref="input_4" class="sign-input" @keyup="handleKeyUpEvent($event)" @keydown="handleKeyDownEvent($event)" type="text" />
+                <input ref="input_5" class="sign-input" @keyup="handleKeyUpEvent($event)" @keydown="handleKeyDownEvent($event)" type="text" />
+                <input ref="input_6" class="sign-input" @keyup="handleKeyUpEvent($event)" @keydown="handleKeyDownEvent($event)" type="text" />
+            </div>
+            <div class="flex items-center gap-5">
+                <div class="text-white">Insert or paste lobby code to join the game!</div>
+                <Button @click="handleJoinLobbyClick" icon="pi pi-play" label="Join" size="small" outlined />
+            </div>
         </div>
     </div>
 </template>
 
-<script lang="ts">
-export default {
-    setup() {
-        const lobby_name = ref("");
-        const input_parent = ref<HTMLInputElement | null>(null);
-        const input_1 = ref<HTMLInputElement | null>(null);
-        const input_2 = ref<HTMLInputElement | null>(null);
-        const input_3 = ref<HTMLInputElement | null>(null);
-        const input_4 = ref<HTMLInputElement | null>(null);
-        const input_5 = ref<HTMLInputElement | null>(null);
-        const input_6 = ref<HTMLInputElement | null>(null);
-        let inputs_array: Ref<HTMLInputElement | null>[] = [];
+<script setup lang="ts">
+const input_parent = ref<HTMLInputElement | null>(null);
+const input_1 = ref<HTMLInputElement | null>(null);
+const input_2 = ref<HTMLInputElement | null>(null);
+const input_3 = ref<HTMLInputElement | null>(null);
+const input_4 = ref<HTMLInputElement | null>(null);
+const input_5 = ref<HTMLInputElement | null>(null);
+const input_6 = ref<HTMLInputElement | null>(null);
+let inputs_array: Ref<HTMLInputElement | null>[] = [];
+const router = useRouter();
+const auth = useAuthenticationService().value;
+const isLoginDialogVisible = useIsLoginDialogVisible();
+const lobbyURLParameter = ref("");
+const toast = useToast();
 
-        onMounted(() => {
-            input_1.value?.focus(); // Focus first input
-            inputs_array = [input_1, input_2, input_3, input_4, input_5, input_6]; // Create array of inputs for logic\
+onMounted(() => {
+    input_1.value?.focus(); // Focus first input
+    inputs_array = [input_1, input_2, input_3, input_4, input_5, input_6]; // Create array of inputs for logic\
 
-            handePasteEvent(); // Activate paste event
+    handePasteEvent(); // Activate paste event
+
+    // Check if there is lobby_id in url
+    const lobbyIdParam = router.currentRoute.value.query.id;
+    lobbyURLParameter.value = Array.isArray(lobbyIdParam) ? lobbyIdParam[0] || "" : lobbyIdParam || "";
+    console.log(lobbyURLParameter.value);
+
+    if (lobbyURLParameter.value) {
+        const lobby_id = lobbyURLParameter.value.split("").slice(0, 6).join("");
+        inputs_array.forEach((input, id) => {
+            if (input.value) input.value.value = lobby_id[id];
         });
+        if (!auth.isPlayerAuthenticated()) return (isLoginDialogVisible.value = true);
+        handleJoinLobby(lobbyURLParameter.value);
+    }
+});
 
-        const handePasteEvent = () => {
-            // Create paste event listener
-            input_parent.value?.addEventListener("paste", async (event) => {
-                event.preventDefault(); // Prevent default paste
-                const clipboard_data = event.clipboardData; // Get clipboard data
+const handleJoinLobby = async (lobby_id: string) => {
+    try {
+        await checkIfLobby(lobby_id);
+        await joinLobby(lobby_id);
+    } catch (error: unknown) {
+        if (error instanceof Error) {
+            console.log(error.message);
+            inputs_array.forEach((input) => input.value?.classList.add("error-lobby")); // Add error class to inputs
+            toast.add({ severity: "error", summary: "Error Joining Lobby", detail: error.message, life: 2000 });
+        }
+    }
+};
 
-                if (clipboard_data) {
-                    const clipboard_string = clipboard_data.getData("text");
+const handleJoinLobbyClick = async () => {
+    const lobby_id = inputs_array.map((input) => input.value?.value).join("");
+    await handleJoinLobby(lobby_id);
+};
 
-                    // Split data and assign one char to each input
-                    const splitted_data = clipboard_string.split("");
-                    inputs_array.forEach((input, id) => {
-                        if (input.value) input.value.value = splitted_data[id];
-                    });
+const handePasteEvent = async () => {
+    // Create paste event listener
+    input_parent.value?.addEventListener("paste", async (event) => {
+        event.preventDefault(); // Prevent default paste
+        const clipboard_data = event.clipboardData; // Get clipboard data
 
-                    // Try joining a lobby
-                    try {
-                        await checkIfLobby(clipboard_string);
-                        await joinLobby(clipboard_string);
-                    } catch (error: unknown) {
-                        if (error instanceof Error) {
-                            console.log(error.message);
-                            inputs_array.forEach((input) => input.value?.classList.add("error-lobby")); // Add error class to inputs
-                        }
-                    }
-                }
+        if (clipboard_data) {
+            const clipboard_string = clipboard_data.getData("text");
+
+            // Split data and assign one char to each input
+            const splitted_data = clipboard_string.split("");
+            inputs_array.forEach((input, id) => {
+                if (input.value) input.value.value = splitted_data[id];
             });
-        };
 
-        const handleKeyUpEvent = async (event: KeyboardEvent) => {
-            const input_element = event.target as HTMLInputElement;
-            input_element.value = input_element.value.toUpperCase();
+            // Try joining a lobby
+            await handleJoinLobby(clipboard_string);
+        }
+    });
+};
 
-            if (input_1.value?.value && input_2.value?.value && input_3.value?.value && input_4.value?.value && input_5.value?.value && input_6.value?.value) {
-                // Get all inputs text and combine it to one string
-                const lobby_id = input_1.value.value + input_2.value.value + input_3.value.value + input_4.value.value + input_5.value.value + input_6.value.value;
+const handleKeyUpEvent = async (event: KeyboardEvent) => {
+    const input_element = event.target as HTMLInputElement;
+    input_element.value = input_element.value.toUpperCase();
 
-                // Try joining a lobby
-                try {
-                    await checkIfLobby(lobby_id);
-                    await joinLobby(lobby_id);
-                } catch (error: unknown) {
-                    if (error instanceof Error) {
-                        console.log(error.message);
-                        inputs_array.forEach((input) => input.value?.classList.add("error-lobby")); // Add error class to inputs
-                    }
-                }
-            }
+    if (input_1.value?.value && input_2.value?.value && input_3.value?.value && input_4.value?.value && input_5.value?.value && input_6.value?.value) {
+        // Get all inputs text and combine it to one string
+        const lobby_id = input_1.value.value + input_2.value.value + input_3.value.value + input_4.value.value + input_5.value.value + input_6.value.value;
 
-            // Focus next input after input is filled
-            if (input_element.value.length > 0) {
-                const nextInput = input_element.nextElementSibling as HTMLInputElement;
-                if (nextInput) nextInput.focus();
-            }
-        };
+        // Try joining a lobby
+        await handleJoinLobby(lobby_id);
+    }
 
-        const handleKeyDownEvent = (event: KeyboardEvent) => {
-            const input_element = event.target as HTMLInputElement;
+    // Focus next input after input is filled
+    if (input_element.value.length > 0) {
+        const nextInput = input_element.nextElementSibling as HTMLInputElement;
+        if (nextInput) nextInput.focus();
+    }
+};
 
-            // Disables multiple letters in input
-            if (input_element.value.length === 1 && event.key !== "Backspace") event.preventDefault();
+const handleKeyDownEvent = (event: KeyboardEvent) => {
+    const input_element = event.target as HTMLInputElement;
 
-            // Handles backspace
-            if (event.key === "Backspace") {
-                const previous_input = input_element.previousElementSibling as HTMLInputElement;
-                if (previous_input && input_element.value.length === 0) {
-                    previous_input.focus();
-                    previous_input.value = "";
-                }
-                inputs_array.forEach((input) => input.value?.classList.remove("error-lobby")); // Remove error class from inputs
-            }
-        };
-        return { createLobby, joinLobby, lobby_name, input_parent, input_1, input_2, input_3, input_4, input_5, input_6, useLobbySettings, handleKeyUpEvent, handleKeyDownEvent };
-    },
+    // Disables multiple letters in input
+    if (input_element.value.length === 1 && event.key !== "Backspace") event.preventDefault();
+
+    // Handles backspace
+    if (event.key === "Backspace") {
+        const previous_input = input_element.previousElementSibling as HTMLInputElement;
+        if (previous_input && input_element.value.length === 0) {
+            previous_input.focus();
+            previous_input.value = "";
+        }
+        inputs_array.forEach((input) => input.value?.classList.remove("error-lobby")); // Remove error class from inputs
+    }
 };
 </script>
 
 <style scoped>
-.page-container {
-    width: 100%;
-    height: 100vh;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-
-    background-color: var(--p-surface-900);
-}
-
 .sign-input {
     background: transparent;
-    border: 0.0625rem solid #fff;
+    border: 0.15rem solid #fff;
     border-radius: 0.75rem;
     font-size: 3rem;
     font-weight: 700;
