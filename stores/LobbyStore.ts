8f1ddc3589b5c365a064d5f6cd37607f92ba8b83
valenciaga <martin.valencic1999@gmail.com>
2024-11-27 00:25:38 +0100

    update som stuff

--- stores/LobbyStore.ts
+++ stores/LobbyStore.ts
@@ -85,6 +85,7 @@ export const useLobbyStore = defineStore("lobby", () => {
     };
 
     const joinedLobby = (lobbyInfo: LobbyInfo, userId: string) => {
+        //TODO: Do not update lobbySettings if ModifySettings Modal is open!!
         updateNestedLobbySettings(lobbyInfo);
 
         if (!lobbySettings.value) return console.error("Lobby settings are not initialized");
@@ -122,31 +123,31 @@ export const useLobbyStore = defineStore("lobby", () => {
 
     const applyLobbySettings = () => {
         const socketStore = useWebSocketStore();
-        const ls = lobbySettings.value;
-        const lso = lobbySettingsOriginal.value;
-        if (!ls || !lso) return console.error("Lobby settings are not initialized");
+        const ls = lobbySettings;
+        const lso = lobbySettingsOriginal;
+        if (!ls.value || !lso.value) return console.error("Lobby settings are not initialized");
 
         //@ts-ignore If ccList is empty (=wrong input) dont send it so it wont update on server. Empty arrray if every country is selected
-        if (ls.conf.ccList.length === 0) delete lso.conf.ccList;
-        else if (ls.conf.ccList.length === useCountryList().value.length) ls.conf.ccList = [];
+        if (ls.value.conf.ccList.length === 0) delete lso.value.conf.ccList;
+        else if (ls.value.conf.ccList.length === useCountryList().value.length) ls.value.conf.ccList = [];
 
         // If objects are equal delete them from original settings, else update original settings
-        for (const field in lso.conf) {
+        for (const field in lso.value.conf) {
             //@ts-ignore  Nested objects (ccList) are always different so they are stringified and compared as such
-            if (typeof lso.conf[field] === "object") {
+            if (typeof lso.value.conf[field] === "object") {
                 //@ts-ignore
-                if (JSON.stringify(lso.conf[field]) === JSON.stringify(ls.conf[field])) delete lso.conf[field]; //@ts-ignore
-                else lso.conf[field] = ls.conf[field];
+                if (JSON.stringify(lso.value.conf[field]) === JSON.stringify(ls.value.conf[field])) delete lso.value.conf[field]; //@ts-ignore
+                else lso.value.conf[field] = ls.value.conf[field];
             } else {
                 //@ts-ignore
-                if (lso.conf[field] === ls.conf[field]) delete lso.conf[field]; //@ts-ignore
-                else lso.conf[field] = ls.conf[field];
+                if (lso.value.conf[field] === ls.value.conf[field]) delete lso.value.conf[field]; //@ts-ignore
+                else lso.value.conf[field] = ls.value.conf[field];
             }
         }
 
         const settings = {
             command: "update_lobby_settings",
-            conf: { ...lso.conf },
+            conf: { ...lso.value.conf },
         };
 
         socketStore.sendMessage(settings);
