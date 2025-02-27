933ddeb35c4e6cc7757ad7c160bbdbad2753382a
valenciaga <martin.valencic1999@gmail.com>
2025-02-27 15:13:06 +0100

    add dialog to gameplay when lost connection

---
+++ components/Gameplay/LostConnectionDialog.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <Dialog v-model:visible="isConnected" class="w-96" :closable="false" modal header="Connection Lost">
+            <div class="flex flex-col items-center gap-4 p-4">
+                <ConnectionStatus />
+                <ProgressSpinner v-if="socketStore.connectionStatus === 'reconnecting'" />
+                <Button v-if="socketStore.connectionStatus === 'disconnected'" label="Return to Home" severity="danger" @click="handleReturnHomeClick" />
+            </div>
+        </Dialog>
+    </div>
+</template>
+
+<script setup lang="ts">
+const emit = defineEmits(["leaveLobby"]);
+
+const socketStore = useWebSocketStore();
+const isConnected = computed(() => socketStore.connectionStatus !== "connected");
+
+const handleReturnHomeClick = () => emit("leaveLobby");
+</script>
+
+<style scoped>
+.lost-connection {
+    text-align: center;
+    padding: 20px;
+}
+</style>
