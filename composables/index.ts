e80ae3b9509e073df1c21731fb0808a452e29f2f
valenciaga <martin.valencic1999@gmail.com>
2025-03-05 02:00:00 +0100

    - better structure auth - fix servicer worker not allowing joining lobby

--- composables/index.ts
+++ composables/index.ts
@@ -4,3 +4,6 @@ export { useCountryBattleMode } from "./gamemodes/CountryBattleMode";
 export { useTimer } from "./utils/useTimer";
 export { useCountdown } from "./utils/useCountdown";
 export { useStartApp } from "./utils/useStartApp";
+export { parseJwt } from "./utils/parseJwt";
+
+export { authService } from "./services/authService";
