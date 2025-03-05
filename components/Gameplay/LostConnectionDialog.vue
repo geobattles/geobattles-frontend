3a22a6c100af670211835771bc8b74d565c797d5
valenciaga <martin.valencic1999@gmail.com>
2025-03-05 01:35:35 +0100

    add network connection tracker and dialog when connection lost

--- components/Gameplay/LostConnectionDialog.vue
+++ components/Gameplay/LostConnectionDialog.vue
@@ -1,10 +1,26 @@
 <template>
     <div>
-        <Dialog v-model:visible="isConnected" class="w-96" :closable="false" modal header="Connection Lost">
+        <Dialog v-model:visible="isConnected" class="w-96" :closable="false" modal :header="dialogHeader" :pt:header:class="'!m-auto'">
             <div class="flex flex-col items-center gap-4 p-4">
-                <SocketConnectionStatus />
-                <ProgressSpinner v-if="socketStore.connectionState === 'reconnecting'" />
-                <Button v-if="socketStore.connectionState === 'disconnected'" label="Return to Home" severity="danger" @click="handleReturnHomeClick" />
+                <!-- Network connection lost -->
+                <template v-if="!socketStore.isOnline">
+                    <div class="text-center">
+                        <i class="pi pi-wifi text-4xl text-red-500 mb-3"></i>
+                        <p>Your internet connection appears to be down</p>
+                        <p class="text-sm text-gray-500 mt-2">Waiting for connection to reestablish</p>
+                    </div>
+                    <Button variant="outlined" label="Exit game" severity="danger" @click="handleReturnHomeClick" />
+                </template>
+
+                <!-- Socket connection issues -->
+                <template v-else>
+                    <SocketConnectionStatus />
+                    <ProgressSpinner v-if="socketStore.connectionState === 'reconnecting' || socketStore.connectionState === 'connecting'" />
+                    <div v-if="socketStore.connectionState === 'disconnected'">
+                        <p>Lost connection to the game server.</p>
+                        <Button variant="outlined" label="Exit game" severity="danger" @click="handleReturnHomeClick" />
+                    </div>
+                </template>
             </div>
         </Dialog>
     </div>
@@ -14,7 +30,11 @@
 const emit = defineEmits(["leaveLobby"]);
 
 const socketStore = useWebSocketStore();
-const isConnected = computed(() => socketStore.connectionState !== "connected");
+const isConnected = computed(() => socketStore.connectionState !== "connected" || socketStore.isOnline !== true);
+
+const dialogHeader = computed(() => {
+    return !socketStore.isOnline ? "Network Connection Lost" : "Server Connection Lost";
+});
 
 const handleReturnHomeClick = () => emit("leaveLobby");
 </script>
