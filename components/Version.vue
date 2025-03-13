9274e45fc0f963f096d67b6a574926059ccedccc
valenciaga <martin.valencic1999@gmail.com>
2025-03-14 00:00:46 +0100

    add version to about page

---
+++ components/Version.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="version-info">
+        <div class="copyright">@ 2025 GeoBattles</div>
+        <div class="version">Version: {{ version }} from {{ buildTime }}</div>
+    </div>
+</template>
+
+<script setup lang="ts">
+const runtimeConfig = useRuntimeConfig();
+
+const version = ref(runtimeConfig.public.VERSION || "1.0.0");
+const buildTime = ref(runtimeConfig.public.BUILD_DATE || new Date().toISOString());
+</script>
+
+<style scoped>
+.version-info {
+    color: var(--p-gray-500);
+    text-align: center;
+    padding: 0.5rem;
+}
+</style>
--- pages/about.vue
+++ pages/about.vue
@@ -1,22 +1,25 @@
 <template>
     <div>
         <Header />
-        <Panel class="main-content text-sm md:text-base" header="About us" pt:header:class="text-xl">
-            <p>This is an open-source geolocation guessing game called GeoBattles...</p>
-        </Panel>
+        <div class="main-content text-sm md:text-base">
+            <Panel header="About us" pt:header:class="text-xl">
+                <p>This is an open-source geolocation guessing game called GeoBattles.</p>
+            </Panel>
+            <Version class="text-sm lg:text-base" />
+        </div>
     </div>
 </template>
 
-<script>
-export default {
-    name: "AboutPage",
-};
-</script>
+<script setup lang="ts"></script>
 
 <style scoped>
 .main-content {
     max-width: 800px;
     margin: auto;
-    margin-top: 3%;
+    margin-top: 20px;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
 }
 </style>
