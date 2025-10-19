1a1acc5d05c732263614f4602883086ae06b50e2
valenciaga <martin.valencic1999@gmail.com>
2025-10-19 20:55:17 +0200

    fix gajas code

--- pages/index.vue
+++ pages/index.vue
@@ -5,10 +5,9 @@
         <div class="page-container relative z-10 flex-1 h-0 flex flex-col items-center justify-center gap-4 px-4">
             <div class="text-center animate-fade-in mt-4">
                 <h2 class="text-2xl md:text-3xl font-light mb-2 text-gray-600 dark:text-white">Welcome to</h2>
-                <h1 class="text-5xl md:text-6xl font-bold bg-gradient-to-r from-[#90B77D] to-[#42855B] bg-clip-text text-transparent 
-                    dark:from-[#B6E388] dark:to-[#5F8D4E] dark:bg-gradient-to-r dark:bg-clip-text dark:text-transparent
-                    transform hover:scale-105 transition-transform duration-300
-                    drop-shadow-sm dark:drop-shadow-lg">
+                <h1
+                    class="text-5xl md:text-6xl font-bold bg-gradient-to-r from-[#90B77D] to-[#42855B] bg-clip-text text-transparent dark:from-[#B6E388] dark:to-[#5F8D4E] dark:bg-gradient-to-r dark:bg-clip-text dark:text-transparent transform hover:scale-105 transition-transform duration-300 drop-shadow-sm dark:drop-shadow-lg"
+                >
                     GeoBattles
                 </h1>
             </div>
@@ -25,17 +24,22 @@
                     raised
                 />
                 <div class="text-center mt-4">
-                    <span class="text-gray-600 dark:text-white">or join a lobby by <a class="text-[#42855B] hover:text-[#90B77D] dark:text-[#B6E388] dark:hover:text-[#5F8D4E] underline underline-offset-4 cursor-pointer transition-colors" @click="handleJoinByCodeClick()">game code!</a></span>
+                    <span class="text-gray-600 dark:text-white"
+                        >or join a lobby by
+                        <a
+                            class="text-[#42855B] hover:text-[#90B77D] dark:text-[#B6E388] dark:hover:text-[#5F8D4E] underline underline-offset-4 cursor-pointer transition-colors"
+                            @click="handleJoinByCodeClick()"
+                            >game code!</a
+                        ></span
+                    >
                 </div>
             </div>
-                <AnimatedEarth />
+            <SvgsAnimatedEarth />
         </div>
     </div>
 </template>
 
 <script setup lang="ts">
-import AnimatedEarth from '~/components/Svgs/AnimatedEarth.vue';
-
 const isJoiningLobby = ref(false); // Handle loader
 
 // External services
@@ -71,7 +75,6 @@ const handleJoinByCodeClick = () => {
 </script>
 
 <style scoped>
-
 .page-container {
     width: 100%;
     display: flex;
@@ -79,12 +82,10 @@ const handleJoinByCodeClick = () => {
     justify-content: center;
 }
 
-
 .play-button {
     letter-spacing: 4px;
     padding: 15px 10px;
 }
-
 
 .clickable {
     text-decoration: underline;
